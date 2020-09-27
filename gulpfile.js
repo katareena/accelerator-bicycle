8cdc96bcb1edf3b89cb6e6834f7507ac3e4be872
katareena <w10160177@gmail.com>
2020-09-27 21:39:01 +0300

    Добавляет стили для блока О нас и Велосипелы

--- gulpfile.js
+++ gulpfile.js
@@ -95,7 +95,7 @@ gulp.task("server", function () {
     ui: false
   });
 
-  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
+  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   // gulp.watch("source/fonts/**/*.{woff,woff2}", "source/img/**", "source/js/**", gulp.series("copy", "refresh"));
