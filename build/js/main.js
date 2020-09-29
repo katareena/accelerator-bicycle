242fb092e61d94f75bbe9f9283f4561a39f7934c
katareena <w10160177@gmail.com>
2020-09-29 17:57:53 +0300

    Добавляет адаптивную графику

--- build/js/main.js
+++ build/js/main.js
@@ -1,133 +1,142 @@
 'use strict';
 
-// (function () {
-// ------------------ карта ------------------
-function mapInit() {
-  var screens = {
-    sm: 320,
-    md: 768,
-    xl: 1024
-  };
-  var iconSize = {
-    sm: [50, 50],
-    md: [70, 70],
-    xl: [70, 70]
-  };
-  var iconOffset = {
-    sm: [-31, -53],
-    md: [-58, -66],
-    xl: [-58, -66]
-  };
-  var mapCenter = {
-    sm: [59.938631, 30.323055],
-    md: [59.938631, 30.323055],
-    xl: [59.938631, 30.319809]
-  };
-  var mapZoom = {
-    sm: 16,
-    md: 16,
-    xl: 17
-  };
+(function () {
+// ------------------ яндекс карта ------------------
+  // function mapInit() {
+  //   var screens = {
+  //     sm: 320,
+  //     md: 768,
+  //     xl: 1024
+  //   };
+  //   var iconSize = {
+  //     sm: [50, 50],
+  //     md: [70, 70],
+  //     xl: [70, 70]
+  //   };
+  //   var iconOffset = {
+  //     sm: [-31, -53],
+  //     md: [-58, -66],
+  //     xl: [-58, -66]
+  //   };
+  //   var mapCenter = {
+  //     sm: [59.938631, 30.323055],
+  //     md: [59.938631, 30.323055],
+  //     xl: [59.938631, 30.319809]
+  //   };
+  //   var mapZoom = {
+  //     sm: 16,
+  //     md: 16,
+  //     xl: 17
+  //   };
 
-  function getScreenSize() {
-    var documentWidth = document.documentElement.clientWidth;
+  //   function getScreenSize() {
+  //     var documentWidth = document.documentElement.clientWidth;
 
-    if (documentWidth < screens.md) {
-      return 'sm';
-    }
+  //     if (documentWidth < screens.md) {
+  //       return 'sm';
+  //     }
 
-    if (documentWidth < screens.xl) {
-      return 'md';
-    }
+  //     if (documentWidth < screens.xl) {
+  //       return 'md';
+  //     }
 
-    return 'xl';
-  }
+  //     return 'xl';
+  //   }
 
-  var screenSize = getScreenSize();
-  var map = new ymaps.Map(document.querySelector('.contacts__map'), {
-    center: mapCenter[screenSize],
-    zoom: mapZoom[screenSize],
-    controls: ['zoomControl']
-  });
-  map.container.fitToViewport();
-  map.behaviors.disable('scrollZoom');
+  //   var screenSize = getScreenSize();
+  //   var map = new ymaps.Map(document.querySelector('.contacts__map'), {
+  //     center: mapCenter[screenSize],
+  //     zoom: mapZoom[screenSize],
+  //     controls: ['zoomControl']
+  //   });
+  //   map.container.fitToViewport();
+  //   map.behaviors.disable('scrollZoom');
 
-  function createMarker(screenWidth) {
-    return new ymaps.Placemark([59.938631, 30.323055], {}, {
-      iconLayout: 'default#image',
-      iconImageHref: 'img/map-pin.png',
-      iconImageSize: iconSize[screenWidth],
-      iconImageOffset: iconOffset[screenWidth]
-    });
-  }
+  //   function createMarker(screenWidth) {
+  //     return new ymaps.Placemark([59.938631, 30.323055], {}, {
+  //       iconLayout: 'default#image',
+  //       iconImageHref: 'img/map-pin.png',
+  //       iconImageSize: iconSize[screenWidth],
+  //       iconImageOffset: iconOffset[screenWidth]
+  //     });
+  //   }
 
-  var marker = createMarker(screenSize);
-  map.geoObjects.add(marker);
-  map.events.add('sizechange', function (event) {
-    var oldWidth = event.get('oldSize')[0];
-    var newWidth = event.get('newSize')[0];
+  //   var marker = createMarker(screenSize);
+  //   map.geoObjects.add(marker);
+  //   map.events.add('sizechange', function (event) {
+  //     var oldWidth = event.get('oldSize')[0];
+  //     var newWidth = event.get('newSize')[0];
 
-    function updateMarker() {
-      map.geoObjects.remove(marker);
-      marker = createMarker(getScreenSize());
-      map.geoObjects.add(marker);
-    }
+  //     function updateMarker() {
+  //       map.geoObjects.remove(marker);
+  //       marker = createMarker(getScreenSize());
+  //       map.geoObjects.add(marker);
+  //     }
 
-    if (oldWidth < screens.md && newWidth >= screens.md) {
-      updateMarker();
-    }
+  //     if (oldWidth < screens.md && newWidth >= screens.md) {
+  //       updateMarker();
+  //     }
 
-    if (oldWidth >= screens.md && newWidth < screens.md) {
-      updateMarker();
-    }
+  //     if (oldWidth >= screens.md && newWidth < screens.md) {
+  //       updateMarker();
+  //     }
 
-    if (oldWidth < screens.xl && newWidth >= screens.xl) {
-      map.setCenter(mapCenter.xl, mapZoom.xl);
-    }
+  //     if (oldWidth < screens.xl && newWidth >= screens.xl) {
+  //       map.setCenter(mapCenter.xl, mapZoom.xl);
+  //     }
 
-    if (oldWidth >= screens.xl && newWidth < screens.xl) {
-      map.setCenter(mapCenter.md, mapZoom.md);
-    }
-  });
-}
+  //     if (oldWidth >= screens.xl && newWidth < screens.xl) {
+  //       map.setCenter(mapCenter.md, mapZoom.md);
+  //     }
+  //   });
+  // }
 
-ymaps.ready(mapInit);
+  // ------------------ валидация телефона ------------------
+  // function maskPhone(selector, masked = '+7 (___) ___-__-__') {
+  //   const elems = document.querySelectorAll(selector);
 
-// ------------------ валидация телефона ------------------
-var maskedInputs = document.querySelectorAll('[data-mask]');
+  //   function mask(event) {
+  //     const keyCode = event.keyCode;
+  //     const template = masked,
+  //     def = template.replace(/\D/g, ""),
+  //       val = this.value.replace(/\D/g, "");
+  //     console.log(template);
+  //     let i = 0,
+  //       newValue = template.replace(/[_\d]/g, function (a) {
+  //         return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+  //       });
+  //     i = newValue.indexOf("_");
+  //     if (i !== -1) {
+  //       newValue = newValue.slice(0, i);
+  //     }
+  //     let reg = template.substr(0, this.value.length).replace(/_+/g,
+  //       function (a) {
+  //         return "\\d{1," + a.length + "}";
+  //       }).replace(/[+()]/g, "\\$&");
+  //     reg = new RegExp("^" + reg + "$");
+  //     if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
+  //       this.value = newValue;
+  //     }
+  //     if (event.type === "blur" && this.value.length < 5) {
+  //       this.value = "";
+  //     }
 
-for (var index = 0; index < maskedInputs.length; index++) {
-  maskedInputs[index].addEventListener('input', validationPhone);
-}
+  //   }
 
-function validationPhone() {
-  var input = this;
-  var mask = input.dataset.mask;
-  var value = input.value;
-  var literalPattern = /[0\*]/;
-  var numberPattern = /[0-9]/;
-  var newValue = '';
-  try {
-    var maskLength = mask.length;
-    var valueIndex = 0;
-    var maskIndex = 0;
+  //   for (const elem of elems) {
+  //     elem.addEventListener("input", mask);
+  //     elem.addEventListener("focus", mask);
+  //     elem.addEventListener("blur", mask);
+  //   }
 
-    for (; maskIndex < maskLength;) {
-      if (maskIndex >= value.length) break;
+  // }
 
-      if (mask[maskIndex] === '0' && value[valueIndex].match(numberPattern) === null) break;
+  // window.main = {
+  //   maskPhone: maskPhone,
+  //   mapInit: mapInit
+  // };
 
-      // Found a literal
-      while (mask[maskIndex].match(literalPattern) === null) {
-        if (value[valueIndex] === mask[maskIndex]) break;
-        newValue += mask[maskIndex++];
-      }
-      newValue += value[valueIndex++];
-      maskIndex++;
-    }
+  // window.main.maskPhone('#phone');
+  // ymaps.ready(window.main.mapInit);
 
-    input.value = newValue;
-  } catch (e) {
-    console.log(e);
-  }
-}
+})();
--- gulpfile.js
+++ gulpfile.js
@@ -1,132 +1,101 @@
-"use strict";
+'use strict';
 
-var gulp = require("gulp");
-var server = require("browser-sync").create();
-var plumber = require("gulp-plumber");
-var rename = require("gulp-rename");
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var sourcemap = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var server = require('browser-sync').create();
+var csso = require('gulp-csso');
+var rename = require('gulp-rename');
+var imagemin = require('gulp-imagemin');
+var webp = require('gulp-webp');
+var svgstore = require('gulp-svgstore')
+var posthtml = require('gulp-posthtml');
+var include = require('posthtml-include');
+var del = require('del');
 
-var del = require("del");
-var sourcemap = require("gulp-sourcemaps");
-var sass = require("gulp-sass");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var csso = require("gulp-csso");
-
-
-var imagemin = require("gulp-imagemin");
-var webp = require("gulp-webp");
-var svgstore = require("gulp-svgstore")
-var posthtml = require("gulp-posthtml");
-var include = require("posthtml-include");
-
-var concat = require("gulp-concat");
-var uglify = require("gulp-uglify");
-var gulpIf = require("gulp-if");
-
-gulp.task("clean", function () {
-  return del("build");
+gulp.task('css', function () {
+  return gulp.src('source/sass/style.scss')
+      .pipe(plumber())
+      .pipe(sourcemap.init())
+      .pipe(sass())
+      .pipe(postcss([ autoprefixer() ]))
+      .pipe(csso())
+      .pipe(rename('style.min.css'))
+      .pipe(sourcemap.write('.'))
+      .pipe(gulp.dest('build/css'))
+      .pipe(server.stream());
 });
 
-gulp.task("html", function () {
-  return gulp.src("source/*.html")
-    .pipe(posthtml([
-      include()
-    ]))
-    .pipe(gulp.dest("build"));
-});
-
-gulp.task("copy", function () {
-  return gulp.src([
-    "source/fonts/**/*.{woff,woff2}",
-    "source/img/**",
-    "source/js/**",
-    "source//*.ico"
-    ], {
-      base: "source"
-    })
-  .pipe(gulp.dest("build"));
-});
-
-gulp.task("webp", function () {
-  return gulp.src("source/img/**/*.{png,jpg}")
-    .pipe(webp({quality: 90}))
-    .pipe(gulp.dest("source/img"));
-});
-
-gulp.task("images", function() {
-  return gulp.src("source/img/**/*.{png,jpg,svg}")
-    .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true}),
-      imagemin.svgo()
-    ]))
-
-    .pipe(gulp.dest("source/img"));
-
-});
-
-gulp.task("sprite", function () {
-  return gulp.src("source/img/{icon-*,htmlacademy*}.svg")
-    .pipe(svgstore({inlineSvg: true}))
-    .pipe(rename("sprite_auto.svg"))
-    .pipe(gulp.dest("build/img"));
-});
-
-gulp.task("css", function () {
-  return gulp.src("source/sass/style.scss")
-    .pipe(plumber())
-    .pipe(sourcemap.init())
-    .pipe(sass())
-    .pipe(postcss([ autoprefixer() ]))
-    .pipe(csso())
-    .pipe(rename("style.min.css"))
-    .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("build/css"))
-    .pipe(server.stream());
-});
-
-
-// gulp.task("js", () => {
-//   return src([
-//     "./node_modules/svg4everybody/dist/svg4everybody.js",
-//     "./node_modules/picturefill/dist/picturefill.js",
-//     "source/js/*.js"
-//   ])
-//     .pipe(gulpIf(sourcemap.init()))
-//     .pipe(concat("bundle.min.js"))
-//     .pipe(gulpIf(sourcemap.write(".")))
-//     .pipe(gulpIf(uglify()))
-//     .pipe(dest("build/js"));
-// });
-
-// gulp.task("js", function () {
-//   return gulp.src("source/js/*.js")
-//     .pipe(gulpIf(sourcemap.init()))
-//     .pipe(concat("bundle.min.js"))
-//     .pipe(gulpIf(sourcemap.write(".")))
-//     .pipe(gulpIf(uglify()))
-//     .pipe(dest("build/js"));
-// });
-
-gulp.task("refresh", function (done) {
-  server.reload();
-  done();
-});
-
-gulp.task("server", function () {
+gulp.task('server', function () {
   server.init({
-    server: "build/",
+    server: 'build/',
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
 
-  gulp.watch("source/*.html", gulp.series("html", "refresh"));
-  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css", "refresh"));
-  gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
-  // gulp.watch("source/js/**/*.js", gulp.series("js", "refresh"));
+  gulp.watch('source/sass/**/*.{scss,sass}', gulp.series('css'));
+  gulp.watch('source/img/icon-*.svg', gulp.series('sprite', 'html', 'refresh'));
+  gulp.watch('source/*.html', gulp.series('html', 'refresh'));
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
-gulp.task("start", gulp.series("build", "server"));
+gulp.task('refresh', function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task('images', function () {
+  return gulp.src('source/img/**/*.{png,jpg,svg}')
+      .pipe(imagemin([
+        imagemin.optipng({optimizationLevel: 3}),
+        imagemin.jpegtran({progressive: true}),
+        imagemin.svgo()
+      ]))
+
+      .pipe(gulp.dest('source/img'));
+
+});
+
+gulp.task('webp', function () {
+  return gulp.src('source/img/**/*.{png,jpg}')
+      .pipe(webp({quality: 90}))
+      .pipe(gulp.dest('source/img'));
+});
+
+gulp.task('sprite', function () {
+  return gulp.src('source/img/{icon-*,htmlacademy*}.svg')
+      .pipe(svgstore({inlineSvg: true}))
+      .pipe(rename('sprite_auto.svg'))
+      .pipe(gulp.dest('build/img'));
+});
+
+gulp.task('html', function () {
+  return gulp.src('source/*.html')
+      .pipe(posthtml([
+        include()
+      ]))
+      .pipe(gulp.dest('build'));
+});
+
+gulp.task('copy', function () {
+  return gulp.src([
+    'source/fonts/**/*.{woff,woff2}',
+    'source/img/**',
+    'source/js/**',
+    'source//*.ico'
+  ], {
+    base: 'source'
+  })
+      .pipe(gulp.dest('build'));
+});
+
+gulp.task('clean', function () {
+  return del('build');
+});
+
+gulp.task('build', gulp.series('clean', 'copy', 'css', 'sprite', 'html'));
+gulp.task('start', gulp.series('build', 'server'));
