c9b354bd61e53e88f2a81d22bdc88979a27a7465
katareena <w10160177@gmail.com>
2020-10-06 19:06:47 +0300

    Добавляет оптимизацию + мелкие правки

--- source/js/main.js
+++ source/js/main.js
@@ -1,48 +1,32 @@
 'use strict';
-
 (function () {
-  // ------------------ валидация телефона ------------------
-  function maskPhone(selector, masked = '+7 (___) ___-__-__') {
-    const elems = document.querySelectorAll(selector);
+  var phoneInput = document.querySelector('#phone');
 
-    function mask(event) {
-      const keyCode = event.keyCode;
-      const template = masked,
-      def = template.replace(/\D/g, ''),
-        val = this.value.replace(/\D/g, '');
-      console.log(template);
-      let i = 0,
-        newValue = template.replace(/[_\d]/g, function (a) {
-          return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
-        });
-      i = newValue.indexOf('_');
-      if (i !== -1) {
-        newValue = newValue.slice(0, i);
-      }
-      let reg = template.substr(0, this.value.length).replace(/_+/g,
-        function (a) {
-          return '\\d{1,' + a.length + '}';
-        }).replace(/[+()]/g, '\\$&');
-      reg = new RegExp('^' + reg + '$');
-      if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
-        this.value = newValue;
-      }
-      if (event.type === 'blur' && this.value.length < 5) {
-        this.value = '';
-      }
-
+  var maskPhoneHeandler = function (evt) {
+    if (!(evt.key === 'ArrowLeft' || evt.key === 'ArrowRight' || evt.key === 'Backspace' || evt.key === 'Tab')) {
+      evt.preventDefault();
     }
-
-    for (const elem of elems) {
-      elem.addEventListener('input', mask);
-      elem.addEventListener('focus', mask);
-      elem.addEventListener('blur', mask);
+    var mask = '+7 (111) 111-11-11';
+    if (/[0-9\+\ \-\(\)]/.test(evt.key)) {
+      var currentString = this.value;
+      var currentLength = currentString.length;
+      if (/[0-9]/.test(evt.key)) {
+        if (mask[currentLength] === '1') {
+          this.value = currentString + evt.key;
+        } else {
+          for (var i = currentLength; i < mask.length; i++) {
+            if (mask[i] === '1') {
+              this.value = currentString + evt.key;
+              break;
+            }
+            currentString += mask[i];
+          }
+        }
+      }
     }
-
-  }
-
+  };
   // ------------------ меню ------------------
-  var ESCAPE = 'Escape';
+  var ESCAPE = 27;
   var burger = document.querySelector('.header__burger');
   var nav = document.querySelector('.header__nav');
 
@@ -57,7 +41,7 @@
   };
 
   var closeEscMenuHeander = function (evt) {
-    if (evt.key === ESCAPE) {
+    if (evt.keyCode === ESCAPE) {
       if (nav.classList.contains('header__nav--open')) {
         document.querySelector('.header__nav').classList.remove('header__nav--open');
         document.querySelector('.header__burger').classList.remove('header__burger--close');
@@ -66,6 +50,7 @@
   };
 
   var closeOutMenuHeander = function (evt) {
+    window.vendor.changeMatchesForIE();
     if (!evt.target.matches('.header__burger')) {
       var dropdowns = document.getElementsByClassName('header__nav');
       var i;
@@ -80,17 +65,16 @@
   };
 
   window.main = {
-    maskPhone: maskPhone,
+    maskPhoneHeandler: maskPhoneHeandler,
     switchMenuHeandler: switchMenuHeandler,
     hiddenMenuHeandler: hiddenMenuHeandler,
     closeOutMenuHeander: closeOutMenuHeander,
     closeEscMenuHeander: closeEscMenuHeander,
   };
 
-  window.main.maskPhone('#phone');
+  phoneInput.addEventListener('keydown', maskPhoneHeandler);
   window.addEventListener('load', window.main.hiddenMenuHeandler);
   document.addEventListener('click', window.main.closeOutMenuHeander);
   document.addEventListener('keydown', window.main.closeEscMenuHeander);
   burger.addEventListener('click', window.main.switchMenuHeandler);
-
 })();
