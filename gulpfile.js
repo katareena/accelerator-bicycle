046a71904f03e1910ee6646ef30bb4b6ed82f296
katareena <w10160177@gmail.com>
2020-10-08 23:07:25 +0300

    Добавляет правки после второго ревью и теста

--- gulpfile.js
+++ gulpfile.js
@@ -11,53 +11,33 @@ var csso = require('gulp-csso');
 var rename = require('gulp-rename');
 var imagemin = require('gulp-imagemin');
 var webp = require('gulp-webp');
-var svgstore = require('gulp-svgstore')
+var svgstore = require('gulp-svgstore');
 var posthtml = require('gulp-posthtml');
 var include = require('posthtml-include');
 var del = require('del');
+var concat = require('gulp-concat');
 
-gulp.task('csscopy', function () {
-  return gulp.src('source/sass/style.scss')
-      .pipe(plumber())
-      .pipe(sourcemap.init())
-      .pipe(sass())
-      .pipe(postcss([ autoprefixer() ]))
-      .pipe(sourcemap.write('.'))
-      .pipe(gulp.dest('build/css'))
-      .pipe(server.stream());
+gulp.task('clean', function () {
+  return del('build');
 });
 
-gulp.task('css', function () {
-  return gulp.src('source/sass/style.scss')
-      .pipe(plumber())
-      .pipe(sourcemap.init())
-      .pipe(sass())
-      .pipe(postcss([ autoprefixer() ]))
-      .pipe(csso())
-      .pipe(rename('style.min.css'))
-      .pipe(sourcemap.write('.'))
-      .pipe(gulp.dest('build/css'))
-      .pipe(server.stream());
+gulp.task('html', function () {
+  return gulp.src('source/*.html')
+      .pipe(posthtml([
+        include()
+      ]))
+      .pipe(gulp.dest('build'));
 });
 
-gulp.task('server', function () {
-  server.init({
-    server: 'build/',
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
-  });
-
-  gulp.watch('source/sass/**/*.{scss,sass}', gulp.series('csscopy', 'css'));
-  gulp.watch('source/img/icon-*.svg', gulp.series('sprite', 'html', 'refresh'));
-  gulp.watch('source/*.html', gulp.series('html', 'refresh'));
-  gulp.watch('source/js/**', gulp.series('copyjs', 'refresh'));
-});
-
-gulp.task('refresh', function (done) {
-  server.reload();
-  done();
+gulp.task('copy', function () {
+  return gulp.src([
+    'source/fonts/**/*.{woff,woff2}',
+    'source/img/**',
+    'source//*.ico',
+  ], {
+    base: 'source'
+  })
+      .pipe(gulp.dest('build'));
 });
 
 gulp.task('images', function () {
@@ -85,38 +65,75 @@ gulp.task('sprite', function () {
       .pipe(gulp.dest('build/img'));
 });
 
-gulp.task('html', function () {
-  return gulp.src('source/*.html')
-      .pipe(posthtml([
-        include()
-      ]))
-      .pipe(gulp.dest('build'));
+gulp.task('csscopy', function () {
+  return gulp.src('source/sass/style.scss')
+      .pipe(plumber())
+      .pipe(sourcemap.init())
+      .pipe(sass())
+      .pipe(postcss([autoprefixer()]))
+      .pipe(sourcemap.write('.'))
+      .pipe(gulp.dest('build/css'))
+      .pipe(server.stream());
 });
 
-gulp.task('copy', function () {
+gulp.task('cssmin', function () {
+  return gulp.src('source/sass/style.scss')
+      .pipe(plumber())
+      .pipe(sourcemap.init())
+      .pipe(sass())
+      .pipe(postcss([autoprefixer()]))
+      .pipe(csso())
+      .pipe(rename('style.min.css'))
+      .pipe(sourcemap.write('.'))
+      .pipe(gulp.dest('build/css'))
+      .pipe(server.stream());
+});
+
+gulp.task('jsvendor', function () {
   return gulp.src([
-    'source/fonts/**/*.{woff,woff2}',
-    'source/img/**',
-    'source/js/**',
-    'source//*.ico'
-  ], {
-    base: 'source'
-  })
-      .pipe(gulp.dest('build'));
+    './node_modules/svg4everybody/dist/svg4everybody.js',
+    './node_modules/picturefill/dist/picturefill.js',
+    'source/js/vendors/*.js',
+  ])
+      .pipe(concat('vendor.js'))
+      .pipe(gulp.dest('build/js'));
 });
 
-gulp.task('copyjs', function () {
+gulp.task('jsmain', function () {
   return gulp.src([
-    'source/js/**',
-  ], {
-    base: 'source'
-  })
-      .pipe(gulp.dest('build'));
+    'source/js/main/*.js',
+  ])
+      .pipe(concat('main.js'))
+      .pipe(gulp.dest('build/js'));
 });
 
-gulp.task('clean', function () {
-  return del('build');
+gulp.task('server', function () {
+  server.init({
+    server: 'build/',
+    reloadOnRestart: true,
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
 });
 
-gulp.task('build', gulp.series('clean', 'copy', 'csscopy', 'css', 'sprite', 'html'));
-gulp.task('start', gulp.series('build', 'server'));
+gulp.task('refresh', function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task('watch', function () {
+  gulp.watch('source/sass/**/*.{scss,sass}', gulp.series('csscopy', 'cssmin'));
+  gulp.watch('source/*.html', gulp.series('html', 'refresh'));
+  gulp.watch('source/img/**/*.{jpg,svg,png}', gulp.series('images'));
+  gulp.watch('source/img/icon-*.svg', gulp.series('sprite', 'html', 'refresh'));
+  gulp.watch('source/js/main/*.js', gulp.series('jsmain', 'refresh'));
+  gulp.watch('source/js/vendors/*.js', gulp.series('jsvendor', 'refresh'));
+});
+
+var buildTasks = ['clean', gulp.parallel(['html', 'csscopy', 'cssmin', 'jsvendor', 'jsmain', 'copy', 'sprite'])];
+
+gulp.task('build', gulp.series(buildTasks));
+gulp.task('start', gulp.series('build', gulp.parallel('server', 'watch')));
---
+++ source/js/main/mask-phone.js
@@ -0,0 +1,34 @@
+'use strict';
+(function () {
+  var phoneInput = document.querySelector('#phone');
+
+  var maskPhoneHandler = function (evt) {
+    if (!(evt.key === 'ArrowLeft' || evt.key === 'ArrowRight' || evt.key === 'Backspace' || evt.key === 'Tab')) {
+      evt.preventDefault();
+    }
+    var mask = '+7 (111) 111-11-11';
+    if (/[0-9\+\ \-\(\)]/.test(evt.key)) {
+      var currentString = this.value;
+      var currentLength = currentString.length;
+      if (/[0-9]/.test(evt.key)) {
+        if (mask[currentLength] === '1') {
+          this.value = currentString + evt.key;
+        } else {
+          for (var i = currentLength; i < mask.length; i++) {
+            if (mask[i] === '1') {
+              this.value = currentString + evt.key;
+              break;
+            }
+            currentString += mask[i];
+          }
+        }
+      }
+    }
+  };
+
+  window.main = {
+    maskPhoneHandler: maskPhoneHandler,
+  };
+
+  phoneInput.addEventListener('keydown', maskPhoneHandler);
+})();
